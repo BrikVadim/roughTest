aefa0283cb1063c647945f25966f315637ea85ec
Vadim Brik <Brik99brik@gmail.com>
2017-01-31 21:11:38 +0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const test = (fn) => {
+  
+  const testParams = [undefined, null, NaN, Infinity, -Infinity, 0, 10, -10, 1e100, -1e100, 
+                      '', '10', '10qwe', 'qwe10', true, false, () => 1, {}, {'1': 1}, [],
+                      [1, 2, 3], ['a', 'b', 'c'], '\t', '\n']
+  let errorCounter = 0
+  
+  const testFn = param => {
+    try {
+      fn(param)
+    } catch(e) {
+      errorCounter++
+      console.log(errorCounter, param, e.toString())
+    }
+  }
+  
+  console.log(`${fn.name}: `)
+  
+  testParams.forEach(elemet => testFn(elemet))
+  
+  if (errorCounter === 0) {
+    console.log(`\tОшибок нет\n\tТест пройден!\n`)
+  } else {
+    console.log(`\tТест не пройден!\n\tКол-во ошибок:${errorCounter}\n`)
+  }
+  
+}
